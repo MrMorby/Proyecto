fad6610112f4504c2e29203752c739d522ec5dbb
LuisMalebranH <116691281+LuisMalebranH@users.noreply.github.com>
2023-11-15 01:00:56 -0300

    Add files via upload

---
+++ Proyectov2/src/Equipo/iGrafica.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Equipo;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author taurusbasilisk < at twitch.tv/taurusbsilisk >
+ */
+public class iGrafica {
+    
+    public static void main(String[] args) {
+    ArrayList<Integrante> listaIntegrantes = new ArrayList<>();
+    while (true) {
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre (o 'fin' para salir):");
+
+        // Salir del bucle si se ingresa "fin"
+        if (nombre.equalsIgnoreCase("fin")) {
+            break;
+        }
+
+        // Pedir la edad
+        String edadStr = JOptionPane.showInputDialog("Ingrese su edad:");
+        int edad = Integer.parseInt(edadStr);
+
+        // Crear un objeto Persona y agregarlo a la lista
+        Integrante nuevoIntegrante = new Integrante(nombre, nombre, edad, edad);
+        listaIntegrantes.add(nuevoIntegrante);
+    }
+
+    // Mostrar la lista de personas
+    StringBuilder mensaje = new StringBuilder("Lista de personas:\n");
+    for (Integrante integrante : listaIntegrantes) {
+        mensaje.append(integrante).append("\n");
+    }
+
+    JOptionPane.showMessageDialog(null, mensaje.toString());
+    
+    
+    }
+}
+
