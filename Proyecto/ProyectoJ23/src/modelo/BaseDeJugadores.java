73167aa8f40a10911063de40df35078da9970e73
MrMorby <die.acevedoc@duocuc.cl>
2023-11-20 19:10:32 -0300

    Le meti las clases y tal :b

--- Proyecto/ProyectoJ23/src/modelo/BaseDeJugadores.java
+++ Proyecto/ProyectoJ23/src/modelo/BaseDeJugadores.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 /**
  *
  * @author Diego
  */
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BaseDeJugadores {
-    //aqui conectaremos la BDD
+    
+    
+    
+    //BASE DE DATOS SIMULADA
+    
+    
+    private ArrayList<Jugadores> Jugadores;
+    private ArrayList<Usuario> Usuario;
+    private ArrayList<String> colecciones;
+    
+    public BaseDeJugadores(){
+        this.Jugadores = new ArrayList<>();
+        this.Usuario = new ArrayList<>();
+        this.colecciones = new ArrayList<>();
+        agregarDatos();
+    }
+
+    public ArrayList<Jugadores> getJugadores() {
+        return Jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugadores> Jugadores) {
+        this.Jugadores = Jugadores;
+    }
+
+    public ArrayList<Usuario> getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(ArrayList<Usuario> Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public ArrayList<String> getColecciones() {
+        return colecciones;
+    }
+
+    public void setColecciones(ArrayList<String> colecciones) {
+        this.colecciones = colecciones;
+    }
+    
+    // customs :p
+        private void agregarDatos() {
+
+        //Datos de libros
+        this.Jugadores.add(new Jugadores("Alexis", "Sanchez", "Masculino", 34, 1));
+        this.Jugadores.add(new Jugadores("Arturo", "Vidal", "Masculino", 12, 2));
+        this.Jugadores.add(new Jugadores("Sergio", "Ramos", "Masculino", 42, 3));
+
+        //Datos de Bibliotecarios
+        this.Usuario.add(new Usuario(1, "217529158", "Diego", "Acevedo", "MrMorby", "Diego123"));
+        this.Usuario.add(new Usuario(2, "123123123", "Luis", "Malebran", "", "Luis123"));
+        
+        //Colecciones
+        this.colecciones.add("Reserva");
+        this.colecciones.add("General");
+        this.colecciones.add("Ingeniería");
+        this.colecciones.add("Clásicos");
+        this.colecciones.add("Literatura Juvenil");
+
+    }
+
+    
 }
--- Proyecto/ProyectoJ23/src/modelo/Usuario.java
+++ Proyecto/ProyectoJ23/src/modelo/Usuario.java
@@ -10,4 +10,77 @@ package modelo;
  */
 public class Usuario {
     // AQUI HAREMOS LOS LOG Y ETC
+    private int id;
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private String user;
+    private String contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String rut, String nombre, String apellido, String user, String contraseña) {
+        this.id = id;
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.user = user;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", rut=" + rut + ", nombre=" + nombre + ", apellido=" + apellido + ", user=" + user + ", contrase\u00f1a=" + contraseña + '}';
+    }
+    
+    
 }
